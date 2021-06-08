63d925e3b706c61aa046794914bcf36a38f0c994
Sejal <sejalaggarwal0@gmail.com>
2021-06-08 14:19:47 +0530

    first attempt

---
+++ src/JOptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author COMPUTER
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String incorrect_Username_or_Password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
